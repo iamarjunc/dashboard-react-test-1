27f79835607721c0776217e7707c68fbab43791c
Arjun C <arjuchandrakumar2017@gmail.com>
2023-06-20 11:36:43 +0530

    initial commir

---
+++ src/components/chart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const Chart = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  );
+};
+
+export default Chart;
